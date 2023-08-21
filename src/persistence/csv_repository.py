bc46669f22bab00169c2cf3ab8be2345c2189c8b
javier <javier.sepulveda.verdugo@gmail.com>
2023-08-21 23:18:21 +0200

    feat: delete prints.

--- src/persistence/csv_repository.py
+++ src/persistence/csv_repository.py
@@ -29,21 +29,17 @@ class CsvRepository:
         return False
 
     def create_task(self, task: Task):
-        try:
-            with open(CsvRepository.csv_filename, mode="a", newline="") as file:
-                writer = csv.writer(file)
-                writer.writerow(
-                    [
-                        task.id,
-                        task.title,
-                        task.description,
-                        task.completed,
-                        task.due_date,
-                    ]
-                )
-            print(str(task))
-        except Exception as e:
-            print(f"An error occurred while saving the task to CSV: {str(e)}")
+        with open(CsvRepository.csv_filename, mode="a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                [
+                    task.id,
+                    task.title,
+                    task.description,
+                    task.completed,
+                    task.due_date,
+                ]
+            )
 
     def read_tasks(self) -> list[Task]:
         tasks: list[Task] = []
